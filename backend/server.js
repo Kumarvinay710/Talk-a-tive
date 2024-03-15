7717faf282b74dbb76e22a36d5df263f787eb125
Vinay kumar <vin6287183337@gmail.com>
2024-03-16 01:54:53 +0530

    Created login and signup Routes along with controllers and middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
 const express=require('express');
 const dotenv=require("dotenv");
+const connectDB = require('./config/db.js');
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+// const chatRoutes = require("./routes/chatRoutes");
+// const messageRoutes = require("./routes/messageRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app=express();
+app.use(express.json());
+
 dotenv.config({path:__dirname+'/.env'});
 
+connectDB();
 
-app.get('/',(req,res)=>{
-  
-    res.send("API is Running Successfully");
-})
+app.use('/api/user',userRoutes);
+
+app.use(notFound);
+app.use(errorHandler)
 
 const PORT=process.env.PORT;
 
-app.listen(PORT,console.log(`Server started on port ${PORT}`));
+app.listen(PORT,console.log(`Server started on port ${PORT}`.yellow.bold));
 
 
